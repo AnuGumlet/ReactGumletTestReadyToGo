9e5a36f7ba3ed9e45d4586ed2536654810d9486c
Anuradha Naik <99973149+AnuGumlet@users.noreply.github.com>
2022-08-15 17:30:59 +0530

    Add Gumlet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
+import React, { useEffect } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+const loadGumletScript = () => {
+    return new Promise(function (resolve, reject){
+      // Checks if the script is already loaded on the page
+      if(document.querySelector("script#gumlet-sdk-script")){
+        resolve();
+      }else {
+        window.GUMLET_CONFIG = {
+            hosts: [{
+            current: "testwebsiteadn1.000webhostapp.com",
+            gumlet: "webfolderanu.gumlet.io"
+            }],
+            lazy_load: true
+        };
+        // Loads the script and appends it on the page
+        const script = document.createElement("script");
+        script.src = "https://cdn.jsdelivr.net/npm/gumlet.js@2.1/dist/gumlet.min.js";
+        script.id = "gumlet-sdk-script";
+        script.sync = true;
+        script.onload = () => resolve();
+        document.body.appendChild(script);
+      }
+    });
+  }
+
+  useEffect(() => {
+    loadGumletScript();
+  });
+
+
+     
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <img data-src="https://webfolderanu.gumlet.io/spiritual.jpg" style={{width:"300px"}} />
+
     </div>
   );
 }
